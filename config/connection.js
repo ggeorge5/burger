e12a98b1e0ab58ba53309f8b1a542a63f1a18db0
ggeorge <gmgeorge1993@gmail.com>
2020-01-15 20:52:00 -0600

    set up connection folder

--- config/connection.js
+++ config/connection.js
@@ -5,3 +5,12 @@ connection = mysql.createConnection({
     password: "password",
     database: "burgers_db"
 });
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
