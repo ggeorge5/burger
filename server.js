d0d6ee81ee38f51dabeea802dcf08dc804508185
ggeorge <gmgeorge1993@gmail.com>
2020-01-16 17:50:12 -0600

    added changes to files

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var bodyparcer = require("body-parser");
+var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
 var app = express();
